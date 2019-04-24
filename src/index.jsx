d5821de4a7769e4ac5634b9884466b107047096b
tomon9086 <tomo.mouse.9086@gmail.com>
2019-04-24 18:49:19 +0900

    routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 import React from "react"
 import { render } from "react-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import Top from "./pages/top"
+import Counter from "./pages/counter"
+import NotFound from "./pages/notfound"
 
 class App extends React.Component {
 	render() {
-		return <p>Hello React!</p>
+		return <Router>
+			<Switch>
+				<Route exact path="/" component={Top} />
+				<Route exact path="/counter" component={Counter} />
+				<Route component={NotFound}/>
+			</Switch>
+		</Router>
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
 	entry: src + "/index.jsx",
 	output: {
 		path: dist,
-		filename: "bundle.js"
+		filename: "bundle.js",
+		publicPath: "/"
 	},
 	module: {
 		rules: [
@@ -23,6 +24,9 @@ module.exports = {
 	},
 	resolve: {
 		extensions: [".js", ".jsx"]
+	},
+	devServer: {
+		historyApiFallback: true,
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
